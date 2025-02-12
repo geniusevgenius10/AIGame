fc7453a439537d85c783af8db56f86107e5da4ed
geniusevgenius10 <lis-05@internet.ru>
2025-02-12 13:00:06 +0300

    NUnit(Fitness + Population)
    Тестирование для фитнес-функции и генерации популяции

---
+++ Assets/Script/Unit/FitnessTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class FitnessTests
+{
+    private GameObject agentObject;
+    private Agent agent;
+
+    [SetUp]
+    public void Setup()
+    {
+        agentObject = new GameObject();
+        agent = agentObject.AddComponent<Agent>();
+    }
+
+    [Test]
+    public void Fitness_Increases_Over_Time()
+    {
+        float initialFitness = agent.fitness;
+        agent.Update();
+        Assert.Greater(agent.fitness, initialFitness);
+    }
+
+    [Test]
+    public void Fitness_Decreases_On_Collision()
+    {
+        float initialFitness = agent.fitness;
+        GameObject obstacle = new GameObject();
+        obstacle.tag = "Obstacle";
+
+        Collision2D collision = new Collision2D();
+        agent.OnCollisionEnter2D(collision);
+
+        Assert.Less(agent.fitness, initialFitness);
+    }
+}
---
+++ Assets/Script/Unit/PopulationTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+[TestFixture]
+public class PopulationTests
+{
+    private PopulationManager populationManager;
+
+    [SetUp]
+    public void Setup()
+    {
+        populationManager = new PopulationManager();
+        populationManager.populationSize = 10;
+        populationManager.Start();
+    }
+
+    [Test]
+    public void Population_Size_Is_Correct()
+    {
+        Assert.AreEqual(10, populationManager.agents.Count);
+    }
+
+    [Test]
+    public void Agents_Have_Initialized_Weights()
+    {
+        foreach (var agent in populationManager.agents)
+        {
+            Assert.IsNotNull(agent);
+            Assert.AreEqual(2, agent.inputSize);
+            Assert.AreEqual(2, agent.hiddenSize);
+            Assert.AreEqual(1, agent.outputSize);
+        }
+    }
+}
