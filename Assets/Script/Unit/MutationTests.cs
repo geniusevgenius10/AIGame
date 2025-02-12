7ac31a8a407758d5b8c49561a695bb84038ae10b
geniusevgenius10 <lis-05@internet.ru>
2025-02-12 13:13:51 +0300

    Create MutationTests.cs
    Тест для мутации нейросети

---
+++ Assets/Script/Unit/MutationTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+[TestFixture]
+public class MutationTests
+{
+    private GeneticAlgorithm geneticAlgorithm;
+    private NeuralNetwork network;
+
+    [SetUp]
+    public void Setup()
+    {
+        geneticAlgorithm = new GeneticAlgorithm();
+        network = new NeuralNetwork(2, 2, 1);
+        network.RandomizeWeights();
+    }
+
+    [Test]
+    public void Mutation_Changes_Weights()
+    {
+        float[] oldWeights = network.weights.ToArray();
+        geneticAlgorithm.Mutate(network, 1.0f); // 100% вероятность мутации
+
+        bool weightsChanged = !oldWeights.SequenceEqual(network.weights);
+        Assert.IsTrue(weightsChanged);
+    }
+}
