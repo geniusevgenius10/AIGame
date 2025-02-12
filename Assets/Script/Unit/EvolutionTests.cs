9ea3addd20305db38e03fce472515a973cc47366
geniusevgenius10 <lis-05@internet.ru>
2025-02-12 13:30:33 +0300

    Create NUnitTests(Evolution/TrainingLoop)
    Тесты для тренировочного цикла и тесты для отбора лучших агентов

---
+++ Assets/Script/Unit/EvolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+[TestFixture]
+public class EvolutionTests
+{
+    private EvolutionManager evolutionManager;
+    private List<NeuralNetwork> agents;
+
+    [SetUp]
+    public void Setup()
+    {
+        evolutionManager = new EvolutionManager();
+        agents = new List<NeuralNetwork>
+        {
+            new NeuralNetwork(2, 2, 1) { fitness = 10 },
+            new NeuralNetwork(2, 2, 1) { fitness = 50 },
+            new NeuralNetwork(2, 2, 1) { fitness = 30 }
+        };
+    }
+
+    [Test]
+    public void Best_Agents_Are_Selected()
+    {
+        List<NeuralNetwork> bestAgents = evolutionManager.SelectBestAgents(agents);
+        Assert.AreEqual(2, bestAgents.Count);
+        Assert.AreEqual(50, bestAgents[0].fitness);
+        Assert.AreEqual(30, bestAgents[1].fitness);
+    }
+}
---
+++ Assets/Script/Unit/TrainingLoopTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class TrainingLoopTests
+{
+    private TrainingLoop trainingLoop;
+    private GameObject trainingLoopObject;
+
+    [SetUp]
+    public void Setup()
+    {
+        trainingLoopObject = new GameObject();
+        trainingLoop = trainingLoopObject.AddComponent<TrainingLoop>();
+        trainingLoop.populationManager = new PopulationManager();
+    }
+
+    [Test]
+    public void Generation_Increments_When_All_Agents_Are_Eliminated()
+    {
+        int initialGeneration = trainingLoop.generation;
+
+        GameObject[] allAgents = GameObject.FindObjectsOfType<Agent>();
+        foreach (var agent in allAgents)
+        {
+            GameObject.Destroy(agent.gameObject);
+        }
+
+        trainingLoop.Update();
+        Assert.Greater(trainingLoop.generation, initialGeneration);
+    }
+}
